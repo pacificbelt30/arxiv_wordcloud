68bd8d603e79df9cbe349731dd45c71ee7d7460c
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2023-05-10 21:57:11 +0900

    fix: minor corrections

--- arxiv_api.py
+++ arxiv_api.py
@@ -3,11 +3,28 @@ import re
 import feedparser
 import json
 import pandas as pd
+from enum import Enum
+from typing import List
 
 """ References
 https://info.arxiv.org/help/api/user-manual.html
 https://github.com/amueller/word_cloud
 """
+
+
+# どの要素でソートするか
+class SortBy(Enum):
+    relevance = 'relevance'
+    lastUpdatedDate = 'lastUpdatedDate'
+    submittedDate = 'submittedDate'
+
+
+# 昇順 or 降順
+class SortOrder(Enum):
+    ascending = 'ascending'
+    descending = 'descending'
+
+
 """
 search_query: string
 id_list: comma-delimited string
@@ -16,17 +33,8 @@ max_results: int
 sortBy: 'relevance' | 'lastUpdatedDate' | 'submittedDate'
 sortOrder: 'ascending' | 'descending'
 """
-
-base = "http://export.arxiv.org/api/query"
-cat = "cs.AI"
-start = 0
-max_results = 10
-sort_by = 'lastUpdatedDate'
-sort_order = 'descending'
-
-# Return Atom 1.0 format
 class ArXiv():
-    def __init__(self, base:str, cat:str, start:int, max_results:int, sort_by, sort_order):
+    def __init__(self, base:str='http://export.arxiv.org/api/query', cat:str='cs.AI', start:int=0, max_results:int=20, sort_by:SortBy=SortBy.lastUpdatedDate, sort_order:SortOrder=SortOrder.descending):
         self.base_url = base
         self.cat = cat
         self.start = start
@@ -36,25 +44,26 @@ class ArXiv():
         self.query_txt = ''
     
     @property
-    def query_url(self):
-        return f'{self.base_url}?search_query=cat:{self.cat}{"+AND+all:"+self.query_txt if not self.query_txt == "" else ""}&start={self.start}&max_results={self.max_results}&sortBy={self.sort_by}&sortOrder={self.sort_order}'
+    def query_url(self) -> str:
+        return f'{self.base_url}?search_query=cat:{self.cat}{"+AND+all:"+self.query_txt if not self.query_txt == "" else ""}&start={self.start}&max_results={self.max_results}&sortBy={self.sort_by.value}&sortOrder={self.sort_order.value}'
 
     @property
-    def titles(self):
+    def titles(self) -> List[str]:
         return [paper['title'] for paper in self.response_dict['entries']]
 
     @property
-    def urls(self):
+    def urls(self) -> List[str]:
         return [paper['url'] for paper in self.response_dict['entries']]
 
     @property
-    def summaries(self):
+    def summaries(self) -> List[str]:
         return [paper['summary'] for paper in self.response_dict['entries']]
 
     @property
-    def title_and_summary(self):
+    def title_and_summary(self) -> List[str]:
         return [t+s for t, s in zip(self.titles, self.summaries)]
 
+    # これは使えない
     def get_table(self):
         all_txt = ''
         for txt in self.title_and_summary:
@@ -63,34 +72,36 @@ class ArXiv():
         print(unique_word_list)
         # unique_word_listの引数を取る
 
+        # 各論文に対してその単語があるかどうかのベクトルを作成
         table = []
-        print(table)
         for i in range(len(self.titles)):
             table.append([])
             for j in range(len(unique_word_list)):
                 table[i].append(1 if (unique_word_list[j] in self.title_and_summary[i]) else 0)
-        print(table)
-        # print(unique_word_list)
+
         df = pd.DataFrame(data=table,index=self.titles, columns=unique_word_list)
-        # df = pd.DataFrame(data=table)
         print(df)
         print(df.sum('index').sort_values())
 
+    # arXiv APIへのクエリ
+    # response Atom 1.0 format
     def query(self, query_txt):
         self.query_txt = query_txt if not query_txt == '' else self.query_txt
-        print(self.query_url)
         self.response = requests.get(self.query_url)
         self.response_dict = feedparser.parse(self.response.text)
-
-    def test(self):
-        pass
+        return
 
 
 if __name__ == "__main__":
-    query_txt = ''
-    a = ArXiv(base, cat, start, max_results, sort_by, sort_order)
-    a.query(query_txt)
+    base = "http://export.arxiv.org/api/query"
+    cat = "cs.AI"
+    start = 0
+    max_results = 10
+    sort_by = SortBy.lastUpdatedDate
+    sort_order = SortOrder.descending
 
-    print(a.response)
-    print(json.dumps(feedparser.parse(a.response.text), indent=2))
-    a.get_table()
+    query_txt = ''
+    arxiv = ArXiv(base, cat, start, max_results, sort_by, sort_order)
+    arxiv.query(query_txt)
+
+    arxiv.get_table()
--- word_cloud.py
+++ word_cloud.py
@@ -1,19 +1,49 @@
+import json
+import requests
 from wordcloud import WordCloud
-from arxiv_api import ArXiv
+from arxiv_api import ArXiv, SortBy, SortOrder
 
-base = "http://export.arxiv.org/api/query"
-cat = "cs.AI"
-start = 0
-max_results = 20
-sort_by = 'lastUpdatedDate'
-sort_order = 'descending'
 
-query_txt = 'Watermark'
+# WordCloudを作成して，Discordに通知するクラス
+class Webhook():
+    def __init__(self, query_txt, filename='wordcloud.png'):
+        self.url = '' # クエリを行うWebhookのURL
+        try:
+            with open('.env', 'r') as f:
+                url = json.load(f)['url']
+                print('url:', url)
+                self.url = url
+        except:
+            import traceback
+            traceback.print_exc()
 
-wordcloud = WordCloud(width=400, height=400, scale=1, max_words=200, min_font_size=4, min_word_length=1)
-arxiv = ArXiv(base, cat, start, max_results, sort_by, sort_order)
-arxiv.query(query_txt)
-wordcloud.generate(' '.join(arxiv.titles))
-wordcloud.to_file('test.png')
+        self.filename = filename # WordCloudの保存先
+        self.query_txt = query_txt # クエリテキスト
 
-webhook()
+    # arXivにクエリを行い，WordCloudを作成
+    def create_wordcloud(self):
+        wordcloud = WordCloud(width=400, height=400, scale=1, max_words=200, min_font_size=4, min_word_length=1)
+        arxiv = ArXiv()
+        arxiv.query(query_txt)
+
+        # WordCloud作成
+        wordcloud.generate(' '.join(arxiv.titles))
+        wordcloud.to_file(self.filename)
+        return
+
+    # Discordへ通知を行う
+    def post_wordcloud_to_discord(self):
+        contents = {
+            'username' : 'arXiv WordCloud Notification',
+            'image': self.filename
+        }
+
+        requests.post(self.url, json=contents)
+        return
+
+if __name__ == "__main__":
+    query_txt = 'Watermark'
+
+    wh = Webhook(query_txt)
+    wh.create_wordcloud()
+    wh.post_wordcloud_to_discord()
